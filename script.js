e9e385f5c4bffe074286908a74381471820b9996
Leonardo Pereira <leonardo_human@hotmail.com>
2023-06-23 15:07:06 -0300

    mobile menu update

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const header = document.querySelector('.header');
-const logo = document.querySelector('logo')
+const logo = document.querySelector('#fixed-logo');
+const mobileMenu = document.querySelector('#mobileMenu');
 
 window.addEventListener('scroll', function() {
-    const scrollPosition = window.pageYOffset;
-
-    const scrollThreshold = 5;
-
-    if (scrollPosition > scrollThreshold) {
+    
+    if (scrollY > 0) {
         header.classList.add('fixed-header');
         logo.classList.add('fixed-logo');
+        mobileMenu.classList.add("fixed-menu");
     } else {
         header.classList.remove('fixed-header');
-        logo.classList.add('fixed-logo');
+        logo.classList.remove('fixed-logo');
+        
     }
 });
+
+
+function onClick() {
+    
+}
